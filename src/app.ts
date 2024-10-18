4917e5796e50cc7b71d9312f4cd091d5fcc34672
rauljlle <rauljlle@gmail.com>
2024-10-17 22:54:46 -0300

    adds / as path for appRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import appRoutes from './controller'
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use('/', appRoutes);
 
 app.listen(port, () => {
     console.log(`Teste na porta ${port}`);
