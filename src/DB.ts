cde09612b0e909a3a2d8bdaa4fe5a20acf83a809
rauljlle <rauljlle@gmail.com>
2024-10-17 23:04:58 -0300

    creates a memory DB using singleton pattern

---
+++ src/DB.ts
@@ -0,0 +1,33 @@
+import { Account } from "./model";
+
+export class DB {
+
+    private data: Account[];
+    static #instance: DB;
+
+    private constructor(){
+        this.data = [];
+    }
+
+    insert(account: Account){
+        this.data.push(account)
+    }
+ 
+    findById(id: string): Account | undefined {
+        return this.data.find(account => account.getId() === id);
+    }
+
+    reset(){
+        this.data = []
+    }
+
+
+    //Implements singleton pattern
+    public static get instance(): DB {
+        if (!DB.#instance) {
+            DB.#instance = new DB();
+        }
+        return DB.#instance;
+    }
+
+}
